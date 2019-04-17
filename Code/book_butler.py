502f600e0c1a2f04a42badeece910756b4c028b2
Muhammad Salman Abid <49055716+msalman-abid@users.noreply.github.com>
2019-04-18 00:04:48 +0500

    Added function to create edges
    The added function calculates weight based on similarity between two readers and creates a corresponding edge between them.

--- Code/book_butler.py
+++ Code/book_butler.py
@@ -14,5 +14,35 @@ def openbookfile(name):
         bookdict={}
         for x in F:     #Used to create a dictionary of desired format.
             bookdict[x[0]]=[x[2].split(','),x[1],x[3]]
-        return bookdict
-print(openbookfile(bookfilename))
+     return bookdict
+
+    
+def WeightedEdge_Create(datadict):
+    final_lst=[]
+    for person in datadict.keys():
+        alrdy_traversed = True 
+        books_read=datadict[person]
+        for neighbor in datadict.keys():
+            weight=0
+            if person==neighbor:
+                alrdy_traversed = False
+                continue
+            if alrdy_traversed: continue
+            for n_books in datadict[neighbor].keys():
+                if n_books in books_read.keys():
+                    currentchoice=books_read[n_books]
+                    neighborchoice=datadict[neighbor][n_books]
+                    if currentchoice==neighborchoice:
+                        weight+=2
+                    else:
+                        weight+=1
+            if weight > 0:
+                final_lst.append((person, neighbor, weight))
+
+    return final_lst
+
+G = {'A':{1: True, 2:True, 5: False},
+'B':{2:False, 1: False, 5:False}
+}
+a = WeightedEdge_Create(G)
+print(a)
